dcac28c6a06d8406dc3a700e542044e0fec6149c
NormaTu545 <NormaTu545@gmail.com>
2016-08-03 11:42:55 -0700

    Level Manager
    A straightforward way to handle transitioning to different levels using
    node switching.  Everything in a level is a child of a node, and when
    you switch out the nodes, everything goes with it.

--- levelManager/GameScene.swift
+++ levelManager/GameScene.swift
@@ -9,34 +9,132 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    var contentNode: SKSpriteNode!
+    var button: SKSpriteNode!
+    
+    var oldNode: SKNode?
+    var contentNode2: SKSpriteNode!
+    var contentNode3: SKSpriteNode!
+    
+    var transitionTime: CFTimeInterval = 0.5
+    var level: Int = 0
+    var levels = [SKNode]()  //Empty array of SKNodes
+    var transitioning = false
+    
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!"
-        myLabel.fontSize = 45
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
         
-        self.addChild(myLabel)
+        contentSetUp() //Level 1
+        contentSetUp_2() //Level 2
+        contentSetUp_3() //Level 3
+        moveOn(contentNode)
+        buttonSetUp()
+
+    }
+    
+    func buttonSetUp() {
+        button = SKSpriteNode(color: UIColor.cyanColor(), size: CGSize(width: 50, height: 50))
+        addChild(button)
+        button.zPosition = 999
+        button.position.x = size.width - 40
+        button.position.y = 40
+        button.name = "nextLevelButton"
+    }
+    
+    func contentSetUp() {
+        contentNode = SKSpriteNode(color: UIColor.blackColor(), size: size)
+        
+        contentNode.anchorPoint = CGPoint(x: 0, y: 0)
+        
+        let label = SKLabelNode(fontNamed: "Helvetica")
+        label.text = "Hello"
+        label.position.x = view!.frame.width/2
+        label.position.y = view!.frame.height/2
+        
+        contentNode.addChild(label)
+        levels.append(contentNode)
+        
+    }
+    
+    func contentSetUp_2() {
+        contentNode2 = SKSpriteNode(color: UIColor.blueColor(), size: size)
+        
+        contentNode2.anchorPoint = CGPoint(x: 0, y: 0)
+        
+        let nextLabel = SKLabelNode(fontNamed: "Courier New Bold")
+        nextLabel.text = "Hi there"
+        nextLabel.position.x = size.width/2
+        nextLabel.position.y = size.height/2
+        
+        contentNode2.addChild(nextLabel)
+        levels.append(contentNode2)
+
+    }
+    
+    func contentSetUp_3() {
+        contentNode3 = SKSpriteNode(color: UIColor.darkGrayColor(), size: size)
+        
+        contentNode3.anchorPoint = CGPoint(x: 0, y: 0)
+        
+        let nextLabel = SKLabelNode(fontNamed: "Ariel")
+        nextLabel.text = "Bye bye"
+        nextLabel.position.x = size.width/2
+        nextLabel.position.y = size.height/2
+        
+        contentNode3.addChild(nextLabel)
+        levels.append(contentNode3)
+    }
+
+    
+    func moveOn(node: SKNode) {
+        
+        transitioning = true
+        
+        if let oldNode = oldNode {
+            let bye = SKAction.moveToX(-size.width, duration: transitionTime) //left
+            let remove = SKAction.removeFromParent()
+            
+            oldNode.runAction(SKAction.sequence([bye, remove]))
+        }
+        
+        node.position.x = size.width
+        addChild(node)
+        
+        let moveAction = SKAction.moveToX(0, duration: transitionTime)
+        
+        let done =  SKAction.runBlock { 
+            self.transitioning = false
+        }
+        
+        node.runAction(SKAction.sequence([moveAction, done]))
+        
+        oldNode = node
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
        /* Called when a touch begins */
         
-        for touch in touches {
-            let location = touch.locationInNode(self)
+        let touch = touches.first!
+        let location = touch.locationInNode(self)
+        
+        let node = nodeAtPoint(location)
+        if node.name == "nextLevelButton" {
+            print("next level button pressed")
             
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
+            if transitioning == false {
+                
+                //MARK: Where we change levels
+                
+                level += 1
+                if level == levels.count {
+                    level = 0
+                }
+                
+                
+                moveOn(levels[level])
+            }
         }
+
     }
    
     override func update(currentTime: CFTimeInterval) {
